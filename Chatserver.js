a8a2e3d4339313fe92414dfe80156276e11fa0c1
Sindri Már Sigfússon <sindris10@ru.is>
2013-03-14 17:24:25 +0000

    asdf

--- Chatserver.js
+++ Chatserver.js
@@ -1,4 +1,24 @@
-setTimeout(function (){
-	console.log( "World");
-}, 3000);
-console.log ( "Hello")
+var express = require("express"),
+    app     = express(),
+    port    = parseInt(process.env.PORT, 10) || 8000;
+    
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+var myGlobalData = {
+	rooms: [],
+};
+app.listen(port);
+
+app.get()

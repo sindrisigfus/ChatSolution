b6aa10520d530a7cac6172ccb91ce8db64958671
obmagnusson <obmagnusson@gmail.com>
2013-03-15 12:55:43 +0000

    sdfdf

--- Public/js/App.js
+++ Public/js/App.js
@@ -3,10 +3,11 @@ function IndexController( $scope){
 	$scope.todos = [
 		{title: "Taka til í sokkaskúffunni", done: false},
 		{title: "Taka til í ísskápnuum", done: false},
-		{title: "Taka til í kaffivélinni", done: false}
+		{title: "Taka til í kaffivélinni", done: false},
+		{title: "This is a totally new option okay!", done: false},
+		{title: "This is a totally new option another one okay!", done: false}
 	];
 
 	$scope.title = "Appið mitt"
 	$scope.currentDate = new Date();
-
 }
